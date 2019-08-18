bdf98249039d7bc2050ee8144936f8b75071fabc
Minh Nguyen <ngmikeng@gmail.com>
2019-08-18 23:57:26 +0700

    WIP mock time series data.

--- api-data/index.js
+++ api-data/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const morgan  = require('morgan');
 const app = express();
 const multiLineChartData = require('./json/multi-line-chart.json');
+const MockTimeSeries = require('./lib/mockTimeSeries');
 
 app.use(cors());
 app.use(morgan('combined'));
@@ -13,6 +14,26 @@ app.get('/api', function (req, res) {
 
 app.get('/api/multi-line-chart-data', function (req, res) {
   res.json(multiLineChartData);
+});
+
+app.get('/api/mock-time-series', function (req, res) {
+  const mockTimeSeries = new MockTimeSeries();
+  const numberOfMapping = 5;
+  let mockSerriesData = [];
+  for (let index = 0; index < numberOfMapping; index++) {
+    const mockData = mockTimeSeries.sampleData({ methodType: 'gaussian' });
+    mockSerriesData.push(mockData);
+  }
+  const result = mockSerriesData[0].map((data, objIndex) => {
+    const arrItem = [data.timestamp, data.value];
+    for (let index = 1; index < mockSerriesData.length; index++) {
+      arrItem.push(mockSerriesData[index][objIndex].value);
+    }
+    return arrItem;
+  });
+  res.json({
+    data: result
+  });
 });
 
 const PORT = process.env.PORT || 3000;
